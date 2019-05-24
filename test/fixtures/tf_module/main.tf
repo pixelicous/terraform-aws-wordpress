925b35d9fa90bd87a025f3b289bf68ae522f0391
pixelicous <pixi@pixelabs.net>
2019-05-24 15:57:45 +0300

    update test file module main

---
+++ test/fixtures/tf_module/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "wordpress" {
+  source = "../../.."
+
+  region = "us-east-1"
+
+  jumpbox_ip     = "0.0.0.0"
+  db_password    = "380cccf909"
+  s3_bucket_name = "wordpress-content-${random_string.short.result}"
+  s3_elblogs_bucket_name = "wordpress-elblogs-${random_string.short.result}"
+  ec2_key_name   = "wordpress"
+  ec2_public_key = "${file("${path.root}/../../assets/wordpress.pub")}"
+
+  tags = {
+    Service = "WordPress"
+  }
+  
+  
+  route53_record_name = "wpblog"
+  route53_zone_id     = "zone_id"
+  rds_db_identifier   = "rds-${random_string.short.result}"
+}
+
+resource "random_string" "short" {
+  length = 5
+  special = false
+}

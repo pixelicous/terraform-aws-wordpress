14703a461430d70d6c171f160b0d9810081a7100
pixelicous <pixi@pixelabs.net>
2019-05-25 00:36:49 +0300

    fix vendor bin folder creation at cache step

--- test/travis/scripts/cache_prepare.sh
+++ test/travis/scripts/cache_prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ ! -d "./vendor/bin" ] && mkdir vendor/bin
+[ ! -d "./vendor/bin" ] && mkdir -p vendor/bin
 
 # Install Terraform
 curl "https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip" -o "terraform_0.11.14_linux_amd64.zip"

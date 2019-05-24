bb4bb06934e30b4cee650ab8df5a231c0bd25662
pixelicous <pixi@pixelabs.net>
2019-05-24 23:47:32 +0300

    Add tests if folder exists, change aws-sdk folder to vendor, change rsa key strength

--- test/travis/scripts/cache_prepare.sh
+++ test/travis/scripts/cache_prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir vendor/bin
+[ ! -d "./vendor/bin" ] && mkdir vendor/bin
 
 # Install Terraform
 wget -q https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
--- test/travis/scripts/kitchen.sh
+++ test/travis/scripts/kitchen.sh
@@ -5,28 +5,31 @@ TRAVISCI_LINUX_IPS='nat.gce-us-central1.travisci.net'
 TRAVISCI_IPS="$(dig +short $TRAVISCI_LINUX_IPS | awk '{printf $1"/32,"}' ORS=" ")"
 
 # Add binaries to bin directory
-mkdir -p vendor/bin
+[ ! -d "./vendor/bin" ] && mkdir -p vendor/bin
 export PATH=$PATH:vendor/bin:vendor/aws-sdk/bin
 
 # Install AWS CLI
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 sudo apt-get -y install unzip
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-/usr/local/bin/aws --version
+sudo ./awscli-bundle/install -i /vendor/aws-sdk -b /vendor/aws-sdk/bin/aws
+/vendor/aws-sdk/bin/aws --version
 
 # Authenticate
 # Make a directory to contain the key
-mkdir -p test/assets
+[ ! -d "./test/assets" ] && mkdir -p test/assets
 
-# Generate a 4096 bit RSA key with a blank passphrase in the directory
+# Generate a 2048 bit RSA key with a blank passphrase in the directory
 # Add "-m pem" if running from OSX mojave
-ssh-keygen -b 4096 -C "Kitchen-Terraform AWS provider tutorial" -f test/assets/wordpress -N "" -t rsa
+ssh-keygen -b 2048 -C "Kitchen-Terraform AWS" -f test/assets/wordpress -N "" -t rsa
 
 
 # Run test
 bundle exec kitchen test --destroy always
 KITCHEN_EXIT_CODE=$? #Save exit code to exit with it, but still clean up no matter the code
-if [ $KITCHEN_EXIT_CODE -ne 0 ]; then cat .kitchen/logs/test-suite-centos.log
+if [ $KITCHEN_EXIT_CODE -ne "0" ]
+then
+cat .kitchen/logs/test-suite-centos.log
+fi
 
 exit $KITCHEN_EXIT_CODE

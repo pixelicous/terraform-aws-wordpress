06ed5dafa8fd14629707a5c6d24bca1236a18cdd
pixelicous <pixi@pixelabs.net>
2019-05-24 18:51:27 +0300

    Change TF version

--- test/travis/scripts/cache_prepare.sh
+++ test/travis/scripts/cache_prepare.sh
@@ -3,8 +3,8 @@
 mkdir tools/bin
 
 # Install Terraform
-wget -q https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-unzip terraform_0.11.11_linux_amd64.zip
-rm terraform_0.11.11_linux_amd64.zip
+wget -q https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+unzip terraform_0.11.14_linux_amd64.zip
+rm terraform_0.11.14_linux_amd64.zip
 mv terraform tools/bin/
 

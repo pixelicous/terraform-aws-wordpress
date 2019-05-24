991aafaeb89da4ac4220b6b69e78e422f0042666
pixelicous <pixi@pixelabs.net>
2019-05-24 22:30:42 +0300

    fix cache prepare shell script

--- test/travis/scripts/cache_prepare.sh
+++ test/travis/scripts/cache_prepare.sh
@@ -6,5 +6,5 @@ mkdir vendor/bin
 wget -q https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
 rm terraform_0.11.14_linux_amd64.zip
-mv terraform tools/bin/
+mv terraform vendor/bin/
 

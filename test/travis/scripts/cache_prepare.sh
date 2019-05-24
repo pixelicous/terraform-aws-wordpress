64176d15253267b3b69eb8c5eee1f31a380dee42
pixelicous <pixi@pixelabs.net>
2019-05-25 02:28:59 +0300

    Fix terraform paths on cache prepare step

--- test/travis/scripts/cache_prepare.sh
+++ test/travis/scripts/cache_prepare.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-[ ! -d "$HOME/vendor/bin" ] && mkdir -p $HOME/vendor/bin
+[ ! -d "./vendor/bin" ] && mkdir -p vendor/bin
 
 # Install Terraform
 curl "https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip" -o "terraform_0.11.14_linux_amd64.zip"
 unzip terraform_0.11.14_linux_amd64.zip
 rm terraform_0.11.14_linux_amd64.zip
-mv terraform $HOME/vendor/bin/
+mv terraform vendor/bin/
 
